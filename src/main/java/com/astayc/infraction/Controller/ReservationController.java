762cb0b290b5cd96b96478a1ef4ba9a7d671f9f9
AstaYC PS <baihmohamedamine@gmail.com>
2024-12-10 08:07:34 +0100

    the controller added succesfely

---
+++ src/main/java/com/astayc/infraction/Controller/ReservationController.java
@@ -0,0 +1,21 @@
+package com.astayc.infraction.Controller;
+
+import com.astayc.infraction.DTO.ReservationDTO;
+import com.astayc.infraction.Service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping
+    public ResponseEntity<ReservationDTO> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        ReservationDTO createdReservation = reservationService.createReservation(reservationDTO);
+        return ResponseEntity.ok(createdReservation);
+    }
+}
